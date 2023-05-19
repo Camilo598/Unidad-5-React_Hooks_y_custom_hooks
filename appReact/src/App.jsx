eee119fbaa7861100c93fdbc20696b9568ca6c6d
Nixson Camilo Pinzon <pinzon0930@gmail.com>
2023-05-19 10:33:22 -0500

    Finalizacion de unidad 5

--- appReact/src/App.jsx
+++ appReact/src/App.jsx
@@ -2,36 +2,28 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Saludo from './components/saludo' 
+import { Tarjeta } from './components/Tarjeta'
+import TarjetaInv from './components/TarjetaInv'
+import Button from './components/Button'
+import Contador from './components/Contador'
+import CambioTema from './components/CambioTema'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const buttonPropiedades = {
+    onClick : () => console.log('Ingresando al boton'),
+    label: 'Mostrar en consola',
+    disabled: false
+  }
+
+  const eventoClick = () => console.log('Ingresando al evento Click');
+  const label = 'Evento click';
+  const disabled = false;
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Curso React Groupo 2</h1>
-      <Saludo nombre="card"/>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <Saludo nombre="Mauricio"/>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Contador />
+      <CambioTema/>
+    </div>
   )
 }
 
