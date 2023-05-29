5a4615fcac24430d6392ef5f6722dba9f8d5b1f7
Nixson Camilo Pinzon <pinzon0930@gmail.com>
2023-05-29 12:23:40 -0500

    Finalizacion de proyecto

---
+++ estudiantesApi/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+import TablaEstudiantes from '../components/TablaEstudiantes'
+import EstudianteProvider from '../components/EstudianteProvider'
+import EstudianteLista from '../components/EstudianteLista'
+import EstudianteLinkedin from '../components/EstudianteLinkedin'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        {/*<TablaEstudiantes/>*/}
+        <EstudianteProvider>
+          <EstudianteLista/>
+        </EstudianteProvider>
+      </div>
+    </>
+  )
+}
+
+export default App
